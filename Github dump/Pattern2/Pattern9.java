55dd62874731d0544fb3b308b68e5f547acf0f79
sandeshninawe25 <128138406+sandeshninawe25@users.noreply.github.com>
2023-04-07 11:34:52 +0530

    Add files via upload

---
+++ Github dump/Pattern2/Pattern9.java
@@ -0,0 +1,19 @@
+class Pattern9
+{
+	public static void main(String args[])
+	{
+		for (int i=65;i<=69;i++)			
+		{	for(int k=68; k>=i; k--)
+			{
+				System.out.print(" ");
+			}
+			for (int j =65;j<=i;j++) 	
+			{
+				System.out.print((char)j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+/
